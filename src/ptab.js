6001b963156c1013701d423d4f67cc7be19f56b2
Parker Hancock <parker.d.hancock@gmail.com>
2019-03-12 11:33:02 -0500

    working state

--- src/ptab.js
+++ src/ptab.js
@@ -1,12 +1,12 @@
+require("@babel/polyfill");
 const request = require('request-promise-native');
 
 class PtabModel {
     constructor(data) {
-        var dates = ["filingDate", "accordedFlingDate", "institutionDecisionDate"];
         for (var key in data) {
             if (data.hasOwnProperty(key)) {
                 var field = data[key]
-                if (dates.indexOf(key) > -1) {
+                if (key.includes("Date")) {
                     field = new Date(field);
                 }
                 this[key] = field;
@@ -16,15 +16,14 @@ class PtabModel {
 }
 
 class PtabManager {
-    constructor(params, defaultQuery) {
+    constructor(params) {
         if (typeof params === 'string' || params instanceof String) {
             this.params = {
-                [defaultQuery]: params
+                [this.defaultQuery]: params
             };
         } else {
         this.params = params;
         };
-        this.defaultQuery = defaultQuery
         this.iteratorIndex = 0;
         this.pages = {};
     }
@@ -33,8 +32,7 @@ class PtabManager {
         if (typeof params === 'string' || params instanceof String) {
             params = {[this.defaultQuery]: params}
         }
-        console.log(this.prototype)
-        return this.prototype.call(params)
+        return this.constructor(params)
     }
 
     async getPage(pageNumber) {
@@ -52,53 +50,44 @@ class PtabManager {
     async next() {
         let pageNumber = Math.floor(this.iteratorIndex / 25);
         let index = this.iteratorIndex % 25;
-        let page = await getPage(pageNumber)
-        return new this.modelClass(page[index])
+        this.iteratorIndex++
+        let page = await this.getPage(pageNumber);
+        return new this.modelClass(page.results[index]);
     }
 
     async get(params) {
         let manager = this.filter(params);
-        return manager.next();
+        return await manager.next();
     }
-
-
-
-
 }
 
-class PtabTrial extends PtabModel {};
+class PtabTrial extends PtabModel {
+    documents () {
+        return PtabDocument.objects.filter(this.trialNumber)
+    }
+};
 
 class PtabTrialManager extends PtabManager {
-    constructor(params) {
-        super(params, "trialNumber")
-        this.endpoint = "trials"
-        this.modelClass = PtabTrial
-    }
-
-    /*
-    async get(params) {
-        var manager = new PtabTrialManager(params)
-        return request.get({url: "https://ptabdata.uspto.gov/ptab-api/" + this.endpoint, 
-        qs: manager.params,
-        json: true,
-        }).then(response => {return new PtabTrial(response.results[0])})
-    }
-    */
+    defaultQuery = "trialNumber"
+    endpoint = "trials"
+    modelClass = PtabTrial
 }
 
-class PtabDocumentManager {
+class PtabDocument extends PtabModel {
+    trial() {
+        return PtabDocument.objects.get(this.trialNumber)
+    }
+};
 
+class PtabDocumentManager extends PtabManager {
+    defaultQuery = "trialNumber"
+    endpoint = "documents"
+    modelClass = PtabDocument
 }
-
-
-
-
 PtabTrial.objects = new PtabTrialManager({})
-
-
-
-
+PtabDocument.objects = new PtabDocumentManager({})
 
 module.exports = {
     PtabTrial: PtabTrial,
+    PtabDocument: PtabDocument,
 }
--- test/ptab.test.js
+++ test/ptab.test.js
@@ -1,9 +1,67 @@
 const ptab = require("../src/ptab");
-PtabTrial = ptab.PtabTrial
+const PtabTrial = ptab.PtabTrial
+const PtabDocument = ptab.PtabDocument
+
 test("can get a trial", () => {
+    /* Trial JSON expected
+    {
+      "trialNumber": "IPR2016-00831",
+      "applicationNumber": "09026118",
+      "patentNumber": "6162705",
+      "petitionerPartyName": "Commissariat a l’Energie Atomique et aux Energies Alternatives",
+      "patentOwnerName": "Silicon Genesis Corporation",
+      "inventorName": "FRANCOIS HENLEY",
+      "prosecutionStatus": "Terminated-Settled",
+      "filingDate": "2016-04-01",
+      "accordedFilingDate": "2016-04-01",
+      "institutionDecisionDate": "2016-09-28",
+      "lastModifiedDatetime": "2017-07-06T16:06:59",
+    */
     return PtabTrial.objects.get("IPR2016-00831").then( data => {
         expect(data.trialNumber).toBe("IPR2016-00831");
         expect(data.patentNumber).toBe("6162705");
         expect(data.filingDate.getTime()).toBe(new Date("2016-04-01").getTime());
     })
 });
+
+test("can get documents for a trial", () => {
+    /* Document JSON expected
+      trialNumber: 'IPR2016-00831',
+      sizeInBytes: 3226184,
+      filingParty: 'petitioner',
+      filingDatetime: '2016-04-01T15:59:39',
+      lastModifiedDatetime: '2016-04-06T13:50:24',
+      documentNumber: '1009',
+      title: 'U.S. Provisional Application No. 60/046,276',
+      mediaType: 'application/pdf',
+      id: 230910,
+      type: 'exhibit',
+    */
+    return PtabDocument.objects.get("IPR2016-00831").then( data => {
+        expect(data.trialNumber).toBe("IPR2016-00831")
+        expect(data.title).toBe("U.S. Provisional Application No. 60/046,276")
+        expect(data.sizeInBytes).toBe(3226184)
+        expect(data.filingParty).toBe("petitioner")
+        expect(data.filingDatetime.getTime()).toBe(new Date("2016-04-01T15:59:39").getTime())
+    })
+})
+
+test("can get documents from a trial", () => {
+    return PtabTrial.objects.get("IPR2016-00831").then(data => data.documents().next()).then(data => {
+        expect(data.title).toBe("U.S. Provisional Application No. 60/046,276")
+    })
+})
+
+test("can iterate through documents", () => {
+    return PtabTrial.objects.get("IPR2016-00831").then(trial => {
+        let docManager = trial.documents();
+        let docs = Array(3);
+        for (let i=0; i < 3; i++) {
+            docs[i] = docManager.next()
+        }
+        return Promise.all(docs)
+    }).then(data => {
+        let doc_nums = data.map(doc => doc.documentNumber, data)
+        expect(doc_nums).toEqual(['1009', '3', '1008'])
+    })
+})
