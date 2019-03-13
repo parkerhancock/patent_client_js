ab0a880386ffc90bdb7acc0072f88c683dfefec2
Parker Hancock <parker.d.hancock@gmail.com>
2019-03-13 14:19:10 -0500

    added babel-built node-compatible library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const ptab = require("./src/ptab")
+const ptab = require("./lib/ptab")
 
 module.exports = {
     PtabTrial: ptab.PtabTrial,
     PtabDocument: ptab.PtabDocument,
+    version: "1.1.4",
 }
