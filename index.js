399a9f345602450d44b9534aed696063ea0838ee
Parker Hancock <parker.d.hancock@gmail.com>
2019-03-15 14:11:28 -0500

    added imports for new modules

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const ptab = require("./lib/ptab")
+const assignment = require("./lib/assignment")
+const peds = require("./lib/peds")
 
 module.exports = {
     PtabTrial: ptab.PtabTrial,
     PtabDocument: ptab.PtabDocument,
-    version: "1.1.4",
+    Assignment: assignment.Assignment,
+    USApplication: peds.USApplication,
+    version: "1.1.5",
 }
